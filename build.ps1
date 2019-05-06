a3d83c65c65051fb53280deb4951aa41cd7747cf
fireflycons <34012094+fireflycons@users.noreply.github.com>
2019-05-06 05:57:12 +0100

    Use PSDepend for build dependencies

--- build.ps1
+++ build.ps1
@@ -13,102 +13,25 @@ try
     Write-Host 'Setting up build environment'
     Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-    if (-not (Get-Variable -Name IsWindows -ErrorAction SilentlyContinue))
+    if (-not (Get-Module -ListAvailable PSDepend))
     {
-        $IsWindows = [System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)
+        Install-Module PSDepend -Repository PSGallery -Scope CurrentUser -Force
     }
 
-    if (-not (Get-Variable -Name IsCoreClr -ErrorAction SilentlyContinue))
-    {
-        $IsCoreClr = $PSVersionTable.ContainsKey('PSEdition') -and $PSVersionTable.PSEdition -eq 'Core'
-    }
+    Import-Module PSDepend
 
-    $loadedModules = Get-Module | Select-Object Name, Version
-
-    $sharedModules = @(
-        New-Object PSObject -Property @{
-            Name            = 'psake'
-            RequiredVersion = [Version]'4.7.4'
+    $psDependTags = $(
+        if (Test-Path -Path variable:PSEdition)
+        {
+            $PSEdition
         }
-        New-Object PSObject -Property @{
-            Name            = 'Pester'
-            RequiredVersion = [Version]'4.7.3'
-        }
-        New-Object PSObject -Property @{
-            Name            = 'BuildHelpers'
-            RequiredVersion = [Version]'2.0.7'
-        }
-        New-Object PSObject -Property @{
-            Name            = 'PSDeploy'
-            RequiredVersion = [Version]'1.0.1'
+        else
+        {
+            'Desktop'
         }
     )
 
-    if ($IsWindows -and -not $IsCoreClr)
-    {
-        $requiredModules = Invoke-Command -NoNewScope {
-            $sharedModules
-
-            @(
-                New-Object PSObject -Property @{
-                    Name            = 'platyPS'
-                    RequiredVersion = [Version]'0.12.0'
-                }
-                New-Object PSObject -Property @{
-                    Name            = 'powershell-yaml'
-                    RequiredVersion = [Version]'0.3.5'
-                }
-                New-Object PSObject -Property @{
-                    Name            = 'AWSPowerShell'
-                    RequiredVersion = [Version]'3.3.485.0'
-                }
-            )
-
-        }
-    }
-    else
-    {
-        $requiredModules = Invoke-Command -NoNewScope {
-            $sharedModules
-
-            @(
-                New-Object PSObject -Property @{
-                    Name            = 'AWSPowerShell.netcore'
-                    RequiredVersion = [Version]'3.3.485.0'
-                }
-            )
-        }
-    }
-
-    # List of modules not already loaded
-    $missingModules = Compare-Object -ReferenceObject $requiredModules.Name -DifferenceObject $loadedModules.Name |
-    Where-Object {
-        $_.SideIndicator -eq '<='
-    } |
-    Select-Object -ExpandProperty InputObject
-
-    if ($missingModules)
-    {
-        $installedModules = Get-Module -ListAvailable |
-        Select-Object Name, Version
-
-        $neededModules = $requiredModules |
-        Where-Object {
-            $r = $_
-            -not ($installedModules | Where-Object {
-                    $_.Name -eq $r.Name -and $_.Version -ge $r.RequiredVersion
-                })
-        }
-
-        $neededModules |
-        ForEach-Object {
-            Write-Host "Installing module: $($_.Name) $($_.RequiredVersion)"
-            Install-Module -Name $_.Name -RequiredVersion $_.RequiredVersion -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
-        }
-
-        Write-Host "Importing modules: $($missingModules -join ',')"
-        Import-Module $missingModules
-    }
+    Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force -Tags $psDependTags
 
     Set-BuildEnvironment -ErrorAction SilentlyContinue
 
